539b63d44cb8ae940139f52e7264e6908057e3a3
techietee <mailtomodupe@gmail.com>
2022-09-09 12:39:31 +0100

    adds table rows id

--- src/app.ts
+++ src/app.ts
@@ -2,24 +2,23 @@ const startApp = async () => {
   let pageData;
   let currentPage = 1;
 
-  const fetchPage = async (page: number) => {
-    const response = await fetch(
-      `https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84&page=${page}`
-    );
+  const fetchPage = async (page: number, url: string) => {
+    if (!url) return;
+    const response = await fetch(url);
     const json = await response.json();
 
-    return json.results?.[0];
+    return json.results[0];
   };
 
   const setData = (data: any[]) => {
     const li = data.reduce((prev, curr) => {
       return (
         prev +
-        `<tr>
-        <td>${curr.row}</td>
-        <td>${curr.gender}</td>
-        <td>${curr.age} </td>
-      </tr>`
+        `<tr data-entryid=${curr?.id}>
+          <td>${curr.row}</td>
+          <td>${curr.gender}</td>
+          <td>${curr.age} </td>
+        </tr>`
       );
     }, "");
 
@@ -29,6 +28,13 @@ const startApp = async () => {
     const pageLabelElement = document.querySelector('[data-pageview=""]');
     if (pageLabelElement)
       pageLabelElement.textContent = `Showing Page ${currentPage}`;
+
+    if (currentPage === 1)
+      document
+        .querySelector('[data-prevbtn=""]')
+        ?.setAttribute("disabled", "true");
+    else
+      document.querySelector('[data-prevbtn=""]')?.removeAttribute("disabled");
   };
 
   const nextBtn = document.querySelector('[data-nextbtn=""]');
@@ -39,7 +45,7 @@ const startApp = async () => {
       currentPage += 1;
       setData(pageData[nextPage]);
     } else {
-      const data = await fetchPage(currentPage + 1);
+      const data = await fetchPage(currentPage + 1, pageData?.paging?.next);
       pageData = data;
       currentPage += 1;
       setData(pageData[currentPage]);
@@ -54,14 +60,17 @@ const startApp = async () => {
       currentPage -= 1;
       setData(pageData[prevPage]);
     } else {
-      const data = await fetchPage(currentPage - 1);
+      const data = await fetchPage(currentPage - 1, pageData?.paging?.previous);
       pageData = data;
       currentPage -= 1;
       setData(pageData[currentPage]);
     }
   });
 
-  const data = await fetchPage(1);
+  const data = await fetchPage(
+    1,
+    "https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84"
+  );
   pageData = data;
   setData(pageData[currentPage]);
 };
